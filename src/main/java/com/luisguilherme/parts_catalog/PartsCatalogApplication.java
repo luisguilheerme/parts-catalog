6b338d0887f9667065c649f7bfb2ad36d669d515
Luis Guilherme Maroni <lg.maroni@gmail.com>
2024-09-06 14:47:03 -0300

    Project Created

---
+++ src/main/java/com/luisguilherme/parts_catalog/PartsCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.luisguilherme.parts_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartsCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartsCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisguilherme/parts_catalog/PartsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisguilherme.parts_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
